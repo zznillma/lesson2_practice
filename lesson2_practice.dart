9db86da6a4cc23d0108fba74bb3354d5a7aec3e7
Bektur <zznillma@MacBook-Pro-Bektur.local>
2022-11-17 20:11:38 +0600

    lesson2_practice

---
+++ lesson2_practice.dart
@@ -0,0 +1,96 @@
+void main() {
+  num a = 1;
+  num b = 2;
+  num c = 3;
+  num d = 12.3;
+  num f = 11;
+
+  int a1 = 12;
+  int b1 = 11;
+  int c1 = 21;
+  int d1 = 43;
+  int f1 = 54;
+
+  double a2 = 12.23;
+  double b2 = 22.2;
+  double c2 = 11.02;
+  double d2 = 1.23;
+  double f2 = 44.22;
+
+  String name = 'Bektur';
+  String surname = 'Dzhumabekov';
+  String patronymic = 'Tabaldyevich';
+  String country = 'Kyrgyzstan';
+  String sex = 'male';
+
+  List<int> numbers = [1, 2, 3, 4];
+  List<String> names = ['Bektur', 'Radmir', 'Dastan'];
+  List<bool> isMale = [true, false, true];
+  List dota2 = ['Zeus', 'Chous Knight', 'Monkey King'];
+  List lessons = ['lesson1', 'lesson2', 'lesson3'];
+
+  Map student = {
+    'name': 'Bektur',
+    'age': 18,
+    'sex': 'male'
+  };
+
+  Map salary = {
+    'IT': 10000,
+    'teacher': 1200,
+    'admin': 2399
+  };
+
+  Map classmate = {
+    'Nikita': 'male',
+    'Veronika': 'female',
+    'Nastya': 'trans'
+  };
+
+  Map groupmate = {
+    'Radmir': 18,
+    'Bektur': 18,
+    'Meder': 17
+  };
+
+  Map sport = {
+    'footboll': 22,
+    'basketboll': 10,
+    'hockey': 44
+  };
+
+  var char = '\u0841';
+  var char1 = '\u0841';
+  var char2 = '\u0850';
+  var char3 = '\u0858';
+  var char4 = '\u0C82';
+
+  final name1;
+  name1 = 'Nikita';
+  final name2;
+  name2 = 'Egor';
+  final name3;
+  name3 = 'Roma';
+  final name4;
+  name4 = 'Anton';
+  final name5;
+  name5 = 'Vova';
+
+  const surname1 = 'Smit';
+  const surname2 = 'Bayden';
+  const work1 = 'waiter';
+  const work2 = 'admin';
+  const work3 = 'IT';
+
+  dynamic flower = 'rose';
+  dynamic numbers1 = 21;
+  dynamic isFemale = true;
+  dynamic work4 = 'clown';
+  dynamic pi = 3.14;
+
+  var lastname = 'Romanov'; 
+  var lastname1 = 'Frolov';
+  var lastname2 = 'Zubenko';
+  var age1 = 21;
+  var age2 = 32;
+}
